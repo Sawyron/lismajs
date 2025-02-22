c02f719d82381449a714f19aa2c08c5e5d9d01ae
Sawyron <s4pakvv@gmail.com>
2025-02-22 14:01:18 +0700

    actuallize test

--- src/ExprEvalListener.test.ts
+++ src/ExprEvalListener.test.ts
@@ -1,31 +1,7 @@
 import { Token } from 'antlr4';
-import { ExprEvalListener } from './ExprEvalListener';
 import { walkOnText } from './index';
 import { LismaErrorListener } from './LismaErrorListener';
 import LismaListener from './gen/LismaListener';
-
-describe('ExprWalker', () => {
-  it('should work', () => {
-    const walker = new ExprEvalListener();
-
-    walkOnText(walker, '7 * 2 + 4');
-
-    const output = walker.evaluate();
-    expect(output.length).toBeGreaterThan(0);
-  });
-});
-
-describe('Lex errors', () => {
-  it('should find lex errors', () => {
-    const walker = new ExprEvalListener();
-    const lexerErrorListener = new LismaErrorListener<number>();
-
-    walkOnText(walker, 'abc', { lexerErrorListener });
-    const errors = lexerErrorListener.errors;
-
-    expect(errors.length).toBeGreaterThan(0);
-  });
-});
 
 describe('Syntax errors', () => {
   it('should find syntax errors', () => {
