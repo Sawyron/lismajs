bc44d3a56e0e75a5e61bae3ed7a44ba3347cc0bf
Sawyron <s4pakvv@gmail.com>
2025-03-31 01:05:04 +0700

    refactor

--- src/hybrid-system/Evaluate.ts
+++ src/hybrid-system/Evaluate.ts
@@ -14,42 +14,26 @@ const evaluateHybridSystem = (
   end: number
 ): IntegrationStep[] => {
   const ds = mapHsToDs(system);
-  const values = system.diffVariableNames.map(
-    value => system.table.get(value)!
-  );
-  const stateFromName = new Map<string, State>(
-    system.states.map(state => [state.name, state])
-  );
   const steps: IntegrationStep[] = [];
-  const getCurrentTransitions = (): [string, Transition][] => {
-    return system.states
-      .filter(s => s.name !== system.activeState?.name)
-      .flatMap(state => {
-        const transitions = state.transitions.filter(
-          t => t.from === system.activeState?.name
-        );
-        return transitions.map<[string, Transition]>(t => [state.name, t]);
-      });
-  };
-  let transitions = getCurrentTransitions();
-  let step = { x: 0, values: values };
+  let transitions = getTransitionsToState(system.activeState!, system);
+  let step = {
+    x: 0,
+    values: system.diffVariableNames.map(value => system.table.get(value)!),
+  } as IntegrationStep;
   while (step.x <= end) {
     steps.push(step);
-    for (const [stateName, transition] of transitions) {
+    for (const [state, transition] of transitions) {
       if (transition.predicate.evaluate()) {
-        const state = stateFromName.get(stateName);
         system.activeState = state;
-        transitions = getCurrentTransitions();
-        if (state) {
-          for (const action of state.onEnterExpressions) {
-            if (action instanceof AssignExpression) {
-              action.execute();
-            }
+        transitions = getTransitionsToState(state, system);
+        for (const action of state.onEnterExpressions) {
+          if (action instanceof AssignExpression) {
+            action.execute();
           }
-          system.diffVariableNames.forEach((variable, index) => {
-            step.values[index] = system.table.get(variable)!;
-          });
         }
+        system.diffVariableNames.forEach((variable, index) => {
+          step.values[index] = system.table.get(variable)!;
+        });
         break;
       }
     }
@@ -57,6 +41,18 @@ const evaluateHybridSystem = (
   }
   return steps;
 };
+
+const getTransitionsToState = (
+  state: State,
+  hs: HybridSystem
+): [State, Transition][] =>
+  hs.states
+    .filter(s => s.name !== state.name)
+    .flatMap(state =>
+      state.transitions
+        .filter(t => t.from === state.name)
+        .map<[State, Transition]>(t => [state, t])
+    );
 
 const mapHsToDs = (hs: HybridSystem): DerivativeSystem => {
   return (x, y) => {
@@ -68,8 +64,7 @@ const mapHsToDs = (hs: HybridSystem): DerivativeSystem => {
       hs.table.set(name, y[index]);
     });
     hs.table.set('time', x);
-    const values = state.diffVariables.map(d => d.expression.evaluate());
-    return [...values];
+    return state.diffVariables.map(d => d.expression.evaluate());
   };
 };
 
